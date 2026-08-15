5ea4276709d930224ba566219d8d604e0c7c1574
Amin Hadinezhad <mohammadaminhadinezhad@gmail.com>
2026-08-15 12:35:13 +0330

    update

--- stats.php
+++ stats.php
@@ -132,7 +132,7 @@ if (!isset($_SESSION['auth'])) { ?>
                     <path d="M9 11V13"></path>
                     <path d="M12 7C12 5.11438 12 4.17157 11.4142 3.58579C10.8284 3 9.88562 3 8 3"></path>
                 </svg>
-                پنل ربات تامین فلات
+                تامین فلات
             </h2>
             <?php if (isset($error)) echo "<p class='error'>{$error}</p>"; ?>
             <form method="POST">
@@ -353,7 +353,7 @@ $allSurveys = $db->query("SELECT first_name, rating, created_at FROM surveys ORD
                 <path d="M9 11V13"></path>
                 <path d="M12 7C12 5.11438 12 4.17157 11.4142 3.58579C10.8284 3 9.88562 3 8 3"></path>
             </svg>
-            پنل ربات تامین فلات</h1>
+            تامین فلات</h1>
         <a href="?logout" class="logout">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" color="currentColor" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
                 <path d="M4.39267 4.00087C4 4.61597 4 5.41166 4 7.00304V16.997C4 18.5883 4 19.384 4.39267 19.9991C4.46279 20.109 4.5414 20.2132 4.62777 20.3108C5.11144 20.8572 5.87666 21.0758 7.4071 21.513C8.9414 21.9513 9.70856 22.1704 10.264 21.8417C10.3604 21.7847 10.45 21.7171 10.5313 21.6402C11 21.1965 11 20.3988 11 18.8034V5.19662C11 3.60122 11 2.80351 10.5313 2.35982C10.45 2.28288 10.3604 2.21527 10.264 2.15827C9.70856 1.82956 8.9414 2.0487 7.4071 2.48699C5.87666 2.92418 5.11144 3.14278 4.62777 3.68925C4.5414 3.78684 4.46279 3.89103 4.39267 4.00087Z"></path>
