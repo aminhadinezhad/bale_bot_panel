7cf870df8c4b646ccc29e41645aab58a86caeb76
Amin Hadinezhad <mohammadaminhadinezhad@gmail.com>
2026-08-24 14:20:32 +0330

    update

--- stats.php
+++ stats.php
@@ -764,7 +764,7 @@ $pdfDate = $pdfExists
                 position: fixed;
                 top: 14px;
                 right: 14px;
-                z-index: 1002;
+                z-index: 998;
 
                 width: 44px;
                 height: 44px;
